4f040bbbe969b49da83a97d6b884c44230c13fd2
LeekDuot <leek.duot@students.mq.edu.au>
2023-08-14 18:22:36 +1000

    week04 prac initial commit

--- src/Actor.java
+++ src/Actor.java
@@ -1,14 +1,32 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.ArrayList;
+import java.awt.Polygon;
 
 public abstract class Actor {
   Color color;
   Cell loc;
+  ArrayList<Polygon> polygons; // ArrayList to hold the polygons
 
-  public void paint(Graphics g) {
-    g.setColor(color);
-    g.fillRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
-    g.setColor(Color.GRAY);
-    g.drawRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
+
+  public Actor(Cell loc) {
+    this.loc = loc;
+    polygons = new ArrayList<>();
+}
+
+public void paint(Graphics g) {
+    // g.setColor(color);
+    // g.fillRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
+    // g.setColor(Color.GRAY);
+    // g.drawRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
+
+
+
+    for (Polygon polygon : polygons) {
+      g.setColor(color);
+      g.fillPolygon(polygon);
+      g.setColor(color);
+      g.drawPolygon(polygon);
+    }
   }
 }
